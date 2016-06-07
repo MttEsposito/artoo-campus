3b8a71df4f851b7a61eebe15b6584c549011f3c9
mattia <mattiaespo.me@gmail.com>
2016-06-07 16:23:30 +0000

    subjects init

--- client/subjects/init/app.js
+++ client/subjects/init/app.js
@@ -1,2 +1,19 @@
 // define app module
+var app=angular
+.module('artooinit',[])
+.run(function(){
+    console.info('ilprimo info1');
+});
+//.config()
+angular
+.module('artooinit')
+.run(function(){
+      console.info('my module bla bla bla');
+//})
+//.run(() =>{
+ // console.info('my module bla bla bla');
+});
 // define a run function
+app.run(function(){
+    console.info('blablabla 2');
+});
