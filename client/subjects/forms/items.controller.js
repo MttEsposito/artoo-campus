a22e10dc6aae342f142f07e82591462cb62a978e
mattia <mattiaespo.me@gmail.com>
2016-06-09 10:47:11 +0000

    forms gg3

--- client/subjects/forms/items.controller.js
+++ client/subjects/forms/items.controller.js
@@ -1,3 +1,24 @@
-angular.module('artoo').controller('ItemsCtrl', function ($scope) {
-  $scope.items = [];
+angular.module('artoo').controller('ItemsCtrl', function ($scope, ItemSrv) {
+    $scope.ItemSrv = ItemSrv;
+   
+    $scope.selectItem=(code)=>{
+        $scope.item=angular.copy(ItemSrv.find(code));
+    };
+  //  $scope.selectedItem=ItemSrv.get()[0].code;
+  //  $scope.selectItem($scope.selectedItem);
+    $scope.insert = function(item, form) {
+        ItemSrv.add(item);
+        $scope.item = {};
+        $scope.resetForm(form);
+    };
+    $scope.update=(item,form)=>
+        ItemSrv.update(item);
+
+
+
+    $scope.resetForm = (form) => {
+       
+  //      $scope.item = {};
+  //      form.$setPristine()
+    };
 });
